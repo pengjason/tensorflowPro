35b9af086462aa88bf3eaeabb318e840d6a2bd9d
jason <335326056@qq.com>
2018-07-23 19:31:09 +0800

    create tensorflow learning proj

---
+++ tensorflow_test/first.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+
+
+def get_tf_info():
+    print(tf.__version__)
+    print(tf.__path__)
+
+def matmultest():
+
+    v1 = tf.constant([[2,3]])
+    v2 = tf.constant([[2],[3]])
+    
+    product = tf.matmul(v1,v2)
+    print(product)
+    
+    sess = tf.Session()
+    result = sess.run(product)
+    print(result)
+    sess.close()
+    
+    
+def init_for_loop():
+    num = tf.Variable(0,name="count")
+    new_value = tf.add(num,10)
+    op = tf.assign(num,new_value)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print('init value:',sess.run(num))
+        for i in range(5):
+            sess.run(op)
+            print(sess.run(num))
+   
+def feed_placeholder():
+    input1 = tf.placeholder(tf.float32)
+    input2 = tf.placeholder(tf.float32)
+    new_value = tf.multiply(input1, input2)
+    with tf.Session( ) as sess:
+        print('get feed placeholder')
+        print(sess.run(new_value,feed_dict={input1:23.0,input2:11.0}))
+        
+init_for_loop()
+feed_placeholder()
+print('test tensorflow')
---
+++ tensorflow_test/linearEstimate.py
@@ -0,0 +1,41 @@
+# from https://blog.csdn.net/geyunfei_/article/details/78782804
+
+import tensorflow as tf
+
+W = tf.Variable([.1],dtype=tf.float32)
+b = tf.Variable([-.1],dtype=tf.float32)
+
+x = tf.placeholder(tf.float32)
+
+linear_model = W*x +b
+
+y = tf.placeholder(tf.float32)
+
+loss = tf.reduce_sum(tf.square(linear_model - y))
+
+sess = tf.Session()
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+print(sess.run(W))
+
+print(sess.run(linear_model, {x: [1, 2, 3, 6, 8]}))
+
+fixW = tf.assign(W,[2.])
+fixb = tf.assign(b,[1.])
+sess.run([fixW,fixb])
+
+print(sess.run(loss, {x: [1, 2, 3, 6, 8], y: [4.8, 8.5, 10.4, 21.0, 25.3]}))
+
+
+optimizer = tf.train.GradientDescentOptimizer(0.001)
+train = optimizer.minimize(loss)
+
+x_train = [1, 2, 3, 6, 8]
+y_train = [4.8, 8.5, 10.4, 21.0, 25.3]
+
+for i in range(10000):
+    sess.run(train, {x: x_train, y: y_train})
+
+print('W: %s b: %s loss: %s' % (sess.run(W), sess.run(b), sess.run(loss, {x: x_train , y: y_train})))
